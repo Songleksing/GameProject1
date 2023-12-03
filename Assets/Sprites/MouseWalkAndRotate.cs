f0eb8ec4fa236787c4481a4c3daae594dfb5c526
Nongp111 <playgame7157@gmail.com>
2023-12-03 16:10:44 +0700

    commit5

---
+++ Assets/Sprites/MouseWalkAndRotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWalkAndRotate : MonoBehaviour
+{
+    public float speed = 5f; // ปรับความเร็วตามต้องการ
+
+    void Update()
+    {
+        // ตรวจสอบว่าปุ่มเมาส์ซ้ายถูกคลิก
+        if (Input.GetMouseButtonDown(0))
+        {
+            WalkAndRotateLeft();
+        }
+
+        // ตรวจสอบว่าปุ่มเมาส์ขวาถูกคลิก
+        else if (Input.GetMouseButtonDown(1))
+        {
+            WalkAndRotateRight();
+        }
+    }
+
+    void WalkAndRotateLeft()
+    {
+        // ขยับไปทางซ้าย
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        // หันไปทางซ้าย
+        transform.localScale = new Vector3(-2, 2, 2);
+    }
+
+    void WalkAndRotateRight()
+    {
+        // ขยับไปทางขวา
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        // หันไปทางขวา
+        transform.localScale = new Vector3(2, 2, 2);
+    }
+}
---
+++ Assets/Sprites/MouseWalkAnimation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWalkAnimation : MonoBehaviour
+{
+    private Animator animator;
+    public float speed = 5f; // Adjust the speed as needed
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Check if the left mouse button is pressed
+        if (Input.GetMouseButton(0))
+        {
+            WalkLeft();
+        }
+        // Check if the right mouse button is pressed
+        else if (Input.GetMouseButton(1))
+        {
+            WalkRight();
+        }
+        // No mouse button pressed, stop walking animation
+        else
+        {
+            StopWalking();
+        }
+    }
+
+    void WalkLeft()
+    {
+        // Play the WalkLeft animation
+        animator.Play("WalkLeft");
+
+        // Move the character to the left
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+    }
+
+    void WalkRight()
+    {
+        // Play the WalkRight animation
+        animator.Play("WalkRight");
+
+        // Move the character to the right
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+    }
+
+    void StopWalking()
+    {
+        // Stop the walking animation
+        animator.Play("iDLE");
+    }
+}
